a76062de6e6ed6dd53749e0def22d4eb33261648
Brett Winters <bossyconnor@gmail.com>
2019-08-26 13:03:34 -0700

    Revert "API version 0.3.0"
    This reverts commit 6642ca40fbf835ae034a330be532c856416d5d52.

--- meraki/models/create_organization_action_batch_model.py
+++ meraki/models/create_organization_action_batch_model.py
@@ -16,11 +16,9 @@ class CreateOrganizationActionBatchModel(object):
 
     Attributes:
         confirmed (bool): Set to true for immediate execution. Set to false if
-            the action should be previewed before executing. This property
-            cannot be unset once it is true. Defaults to false.
-        synchronous (bool): Set to true to force the batch to run synchronous.
-            There can be at most 20 actions in synchronous batch. Defaults to
-            false.
+            the action should be previewed before executing.
+        synchronous (bool): Force the batch to run synchronous. There can be
+            at most 20 actions in synchronous batch.
         actions (list of ActionModel): A set of changes to make as part of
             this action (<a
             href='https://developer.cisco.com/meraki/api/#/rest/guides/action-b
